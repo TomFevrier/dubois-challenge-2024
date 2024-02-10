d9252f25f8bc7ec9229d5e5903968dbfa7a7bc42
TomFevrier <tomfevrier@gmail.com>
2024-02-11 00:59:29 +0100

    Updates

---
+++ _app/immutable/chunks/paths.4-pi89Dx.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1d75svl)==null?void 0:s.base)??"/dubois-challenge-2024";var e;const l=((e=globalThis.__sveltekit_1d75svl)==null?void 0:e.assets)??a;export{l as a,a as b};
---
+++ _app/immutable/entry/start.mZRb2RRp.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.U4t7HYZ1.js";export{t as start};
